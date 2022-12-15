72920c652342ff814f5b85f6bd587602053dc995
Miguel Stc <104473270+MiguelStc@users.noreply.github.com>
2022-12-16 00:01:46 +0100

    Add files via upload

--- tab_creation/time_random.py
+++ tab_creation/time_random.py
@@ -17,105 +17,6 @@ from openpyxl import workbook, load_workbook
 from Item import r_fruits_legumes, r_frais, r_dph, r_epicerie, r_liquides
 
 
-def sep():
-    print("-----------------")
-
-
-hours = [
-    0,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13,
-    14,
-    15,
-    16,
-    17,
-    18,
-    19,
-    20,
-    21,
-    22,
-    23,
-]
-minutes = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    12,
-    13,
-    14,
-    15,
-    16,
-    17,
-    18,
-    19,
-    20,
-    21,
-    22,
-    23,
-    24,
-    25,
-    26,
-    27,
-    28,
-    29,
-    30,
-    31,
-    32,
-    33,
-    34,
-    35,
-    36,
-    37,
-    38,
-    39,
-    40,
-    41,
-    42,
-    43,
-    44,
-    45,
-    46,
-    47,
-    48,
-    49,
-    50,
-    51,
-    52,
-    53,
-    54,
-    55,
-    56,
-    57,
-    58,
-    59,
-]
-
-# generate hours
-
-
-# end def
-for lines in range(1):
-    random_hour = random.randint(0, 22)
-    random_minut = random.randint(0, 57)
-
-
 # declaration rayon
 rayons = [r_fruits_legumes, r_dph, r_epicerie, r_liquides, r_frais]
 # ---------------------------------
@@ -131,72 +32,21 @@ nb_rrv_max = len(rrv) - 1
 nb_sex_max = len(sexe) - 1
 nb_cp_max = len(liste_CP) - 1
 
-# client profile gen
-# name gen
-random_prenom = random.randint(0, nb_prenom_max)
-current_prenom = liste_prenom[random_prenom]
-# last name gen
-random_nom = random.randint(0, nb_nom_max)
-current_nom = liste_nom[random_nom]
-# age gen
-random_age = random.randint(0, nb_age_max)
-current_age = liste_age[random_age]
-# email creation----
-random_atmail = random.randint(0, nb_type_mail_max)
-current_at_mail = liste_email_at[random_atmail]
-current_email = current_prenom + current_nom
-current_full_mail = current_email + current_at_mail
-# adress gen----
-random_home_nb = random.randint(1, 200)
 
-random_rrv = random.randint(0, nb_rrv_max)
-current_rrv = rrv[random_rrv]
-
-random_adress = random.randint(0, nb_adrs_max)
-current_adress = liste_adrs[random_adress]
-
-random_cp = random.randint(0, nb_cp_max)
-current_cp = liste_CP[random_cp]
-# num tel gen
-random_tel_digit = random.randint(111111, 999999)
-random_tel_digit_str = str(random_tel_digit)
-
-random_tel_sart = random.randint(0, nb_for_gen_tel_max)
-current_tel_start = liste_telephone[random_tel_sart]
-current_full_tel_generated = current_tel_start + random_tel_digit_str
-# -----print test client
-# sex gen-----
-random_sexe = random.randint(0, nb_sex_max)
-current_sexe = sexe[random_sexe]
-# ----------
-sep()
-print(
-    current_prenom.capitalize(),
-    current_nom.capitalize(),
-    current_age,
-    "Ans \n",
-    "Email:",
-    current_full_mail.capitalize(),
-    "\n",
-    "Adresse : ",
-    random_home_nb,
-    current_rrv,
-    current_adress.capitalize(),
-    current_cp,
-    "\n",
-    "Téléphone :",
-    current_full_tel_generated,
-    "\n",
-    "Sexe :",
-    current_sexe,
-)
-sep()
 # ouvrir excel
 wb = load_workbook("data.xlsx")
 ws = wb.active
 
 # SIMULATION d'un achat au hasard
-for loops in range(0, 7):
+for loops in range(0,17567):
+    random_hour = random.randint(0, 23)
+    random_minut = random.randint(0, 59)
+
+    current_hour = random_hour
+    current_hour = str(current_hour)
+    current_minuts = random_minut
+    current_minuts = str(current_minuts)
+    actual_time = current_hour + ":" + current_minuts
 
     random_rayon = random.randint(0, 4)
     current_random_rayon = rayons[random_rayon]
@@ -206,6 +56,46 @@ for loops in range(0, 7):
 
     random_hour = random.randint(0, 22)
     random_minut = random.randint(0, 57)
+    # --- client profile gen
+    # name gen
+    random_prenom = random.randint(0, nb_prenom_max)
+    current_prenom = liste_prenom[random_prenom]
+    # last name gen
+    random_nom = random.randint(0, nb_nom_max)
+    current_nom = liste_nom[random_nom]
+    # age gen
+    random_age = random.randint(0, nb_age_max)
+    current_age = liste_age[random_age]
+    # email creation----
+    random_atmail = random.randint(0, nb_type_mail_max)
+    current_at_mail = liste_email_at[random_atmail]
+    current_email = current_prenom + current_nom
+    current_full_mail = current_email + current_at_mail
+    # adress gen----
+    random_home_nb = random.randint(1, 200)
+
+    random_rrv = random.randint(0, nb_rrv_max)
+    current_rrv = rrv[random_rrv]
+
+    random_adress = random.randint(0, nb_adrs_max)
+    current_adress = liste_adrs[random_adress]
+
+    random_cp = random.randint(0, nb_cp_max)
+    current_cp = liste_CP[random_cp]
+    # num tel gen
+    random_tel_digit = random.randint(111111, 999999)
+    random_tel_digit_str = str(random_tel_digit)
+
+    random_tel_sart = random.randint(0, nb_for_gen_tel_max)
+    current_tel_start = liste_telephone[random_tel_sart]
+    current_full_tel_generated = current_tel_start + random_tel_digit_str
+    # -----print test client
+    # sex gen-----
+    random_sexe = random.randint(0, nb_sex_max)
+    current_sexe = sexe[random_sexe]
+    # ----------
+    # generate_Time
+
     ws.append(
         [
             current_random_rayon[random_element_in_r].nom,
@@ -217,6 +107,17 @@ for loops in range(0, 7):
             current_random_rayon[random_element_in_r].bio,
             current_random_rayon[random_element_in_r].ID,
             current_random_rayon[random_element_in_r].tva,
+            current_prenom.capitalize(),
+            current_nom.capitalize(),
+            current_age,
+            current_full_mail.capitalize(),
+            random_home_nb,
+            current_rrv,
+            current_adress.capitalize(),
+            current_cp,
+            current_full_tel_generated,
+            current_sexe,
+            actual_time,
         ]
     )
 
